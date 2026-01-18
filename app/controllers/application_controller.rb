45855d8588d54b9d2bfd24d4db04f5be9dc9c491
samovar <samovar1214@gmail.com>
2026-01-18 20:44:17 +0900

    ユーザー新規登録画面・編集画面をカスタマイズ
    プロフィール更新用にストロングパラメータを適用

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,5 +10,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[postal_code address introduction])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[postal_code address introduction])
   end
 end
