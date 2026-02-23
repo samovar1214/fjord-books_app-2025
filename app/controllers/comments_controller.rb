7b6fcfe1fe2815ccd3067d43b0c73a6dfa61356c
samovar <samovar1214@gmail.com>
2026-02-23 12:00:47 +0900

    current_userで更新・削除権限を判定するように変更
    併せて不要な条件分岐とrespond toを削除

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,42 +2,26 @@
 
 class CommentsController < ApplicationController
   before_action :set_commentable
-  before_action :set_comment, only: %i[edit update destroy]
-  before_action :correct_user, only: %i[edit update destroy]
+  before_action :set_my_comment, only: %i[edit update destroy]
 
   def create
     @comment = @commentable.comments.new(comment_params)
     @comment.user = current_user
 
-    respond_to do |format|
-      if @comment.save
-        format.html { redirect_to @commentable, notice: t('controllers.common.notice_create', name: Comment.model_name.human) }
-      else
-        @comments = @commentable.comments.includes(:user)
-        format.html { render "#{@commentable.class.model_name.plural}/show", status: :unprocessable_entity }
-      end
-    end
+    @comment.save!
+    redirect_to @commentable, notice: t('controllers.common.notice_create', name: Comment.model_name.human)
   end
 
   def edit; end
 
   def update
-    respond_to do |format|
-      if @comment.update(comment_params)
-        format.html { redirect_to @commentable, notice: t('controllers.common.notice_update', name: Comment.model_name.human) }
-      else
-        @comments = @commentable.comments.includes(:user)
-        format.html { render "#{@commentable.class.model_name.plural}/show", status: :unprocessable_entity }
-      end
-    end
+    @comment.update!(comment_params)
+    redirect_to @commentable, notice: t('controllers.common.notice_update', name: Comment.model_name.human)
   end
 
   def destroy
     @comment.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to @commentable, status: :see_other, notice: t('controllers.common.notice_destroy', name: Comment.model_name.human) }
-    end
+    redirect_to @commentable, status: :see_other, notice: t('controllers.common.notice_destroy', name: Comment.model_name.human)
   end
 
   private
@@ -50,12 +34,8 @@ class CommentsController < ApplicationController
     end
   end
 
-  def set_comment
-    @comment = @commentable.comments.find(params[:id])
-  end
-
-  def correct_user
-    redirect_to @commentable, alert: 'Not authorized.' unless @comment.user == current_user
+  def set_my_comment
+    @comment = current_user.comments.find(params[:id])
   end
 
   def comment_params
--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class ReportsController < ApplicationController
-  before_action :set_report, only: %i[show edit update destroy]
-  before_action :correct_user, only: %i[edit update destroy]
+  before_action :set_report, only: %i[show]
+  before_action :set_my_report, only: %i[edit update destroy]
 
   # GET /reports or /reports.json
   def index
@@ -24,33 +24,20 @@ class ReportsController < ApplicationController
   def create
     @report = current_user.reports.build(report_params)
 
-    respond_to do |format|
-      if @report.save
-        format.html { redirect_to @report, notice: t('controllers.common.notice_create', name: Report.model_name.human) }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-      end
-    end
+    @report.save!
+    redirect_to @report, notice: t('controllers.common.notice_create', name: Report.model_name.human)
   end
 
   # PATCH/PUT /reports/1 or /reports/1.json
   def update
-    respond_to do |format|
-      if @report.update(report_params)
-        format.html { redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human) }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-      end
-    end
+    @report.update!(report_params)
+    redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human)
   end
 
   # DELETE /reports/1 or /reports/1.json
   def destroy
     @report.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to reports_path, status: :see_other, notice: t('controllers.common.notice_destroy', name: Report.model_name.human) }
-    end
+    redirect_to reports_path, status: :see_other, notice: t('controllers.common.notice_destroy', name: Report.model_name.human)
   end
 
   private
@@ -60,12 +47,12 @@ class ReportsController < ApplicationController
     @report = Report.find(params.expect(:id))
   end
 
+  def set_my_report
+    @report = current_user.reports.find(params[:id])
+  end
+
   # Only allow a list of trusted parameters through.
   def report_params
     params.expect(report: %i[title body user_id])
-  end
-
-  def correct_user
-    redirect_to @report, alert: 'Not authorized.' unless @report.user == current_user
   end
 end
