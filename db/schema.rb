4602b59029cb88d4df28974451f9ebdbf7b3daa6
samovar <samovar1214@gmail.com>
2026-02-25 20:56:08 +0900

    commentsのbodyにNOT NULL制約を設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_02_23_015126) do
+ActiveRecord::Schema[8.0].define(version: 2026_02_25_115418) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -50,7 +50,7 @@ ActiveRecord::Schema[8.0].define(version: 2026_02_23_015126) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
-    t.text "body"
+    t.text "body", null: false
     t.string "commentable_type", null: false
     t.integer "commentable_id", null: false
     t.datetime "created_at", null: false
