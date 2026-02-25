b190df73b18539c8c9e00ba4b4e0b8221d9f747f
samovar <samovar1214@gmail.com>
2026-02-25 21:33:40 +0900

    関連を使って不要な親（commentable）の取得処理を削除

--- app/controllers/reports/comments_controller.rb
+++ app/controllers/reports/comments_controller.rb
@@ -1,15 +1,14 @@
 # frozen_string_literal: true
 
 class Reports::CommentsController < ApplicationController
-  before_action :set_commentable
   before_action :set_my_comment, only: %i[edit update destroy]
 
   def create
-    @comment = @commentable.comments.new(comment_params)
+    @comment = Report.find(params[:report_id]).comments.new(comment_params)
     @comment.user = current_user
 
     @comment.save!
-    redirect_to @commentable, notice: t('controllers.common.notice_create', name: Comment.model_name.human)
+    redirect_to @comment.commentable, notice: t('controllers.common.notice_create', name: Comment.model_name.human)
   end
 
   def edit
@@ -18,19 +17,15 @@ class Reports::CommentsController < ApplicationController
 
   def update
     @comment.update!(comment_params)
-    redirect_to @commentable, notice: t('controllers.common.notice_update', name: Comment.model_name.human)
+    redirect_to @comment.commentable, notice: t('controllers.common.notice_update', name: Comment.model_name.human)
   end
 
   def destroy
     @comment.destroy!
-    redirect_to @commentable, status: :see_other, notice: t('controllers.common.notice_destroy', name: Comment.model_name.human)
+    redirect_to @comment.commentable, status: :see_other, notice: t('controllers.common.notice_destroy', name: Comment.model_name.human)
   end
 
   private
-
-  def set_commentable
-    @commentable = Report.find(params[:report_id])
-  end
 
   def set_my_comment
     @comment = current_user.comments.find(params[:id])
