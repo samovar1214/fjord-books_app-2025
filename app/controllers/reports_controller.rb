e1682f1faa07ada4482f02273105aec406a4941c
samovar <samovar1214@gmail.com>
2026-02-25 21:03:58 +0900

    onlyの中身がshowだけのbefore_actionを削除
    併せてコメントも削除

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,26 +1,22 @@
 # frozen_string_literal: true
 
 class ReportsController < ApplicationController
-  before_action :set_report, only: %i[show]
   before_action :set_my_report, only: %i[edit update destroy]
 
-  # GET /reports or /reports.json
   def index
     @reports = Report.includes(:user).order(created_at: :desc).page(params[:page])
   end
 
-  # GET /reports/1 or /reports/1.json
-  def show; end
+  def show
+    @report = Report.find(params.expect(:id))
+  end
 
-  # GET /reports/new
   def new
     @report = Report.new
   end
 
-  # GET /reports/1/edit
   def edit; end
 
-  # POST /reports or /reports.json
   def create
     @report = current_user.reports.build(report_params)
 
@@ -28,13 +24,11 @@ class ReportsController < ApplicationController
     redirect_to @report, notice: t('controllers.common.notice_create', name: Report.model_name.human)
   end
 
-  # PATCH/PUT /reports/1 or /reports/1.json
   def update
     @report.update!(report_params)
     redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human)
   end
 
-  # DELETE /reports/1 or /reports/1.json
   def destroy
     @report.destroy!
     redirect_to reports_path, status: :see_other, notice: t('controllers.common.notice_destroy', name: Report.model_name.human)
@@ -42,16 +36,10 @@ class ReportsController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_report
-    @report = Report.find(params.expect(:id))
-  end
-
   def set_my_report
     @report = current_user.reports.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def report_params
     params.expect(report: %i[title body user_id])
   end
