d5794d8c8b103cceee3f0b0cb2efbf547c2abea6
samovar <samovar1214@gmail.com>
2026-03-09 05:56:05 +0900

    belongs_toのpresence: trueを省略

--- app/models/report_mention.rb
+++ app/models/report_mention.rb
@@ -4,6 +4,5 @@ class ReportMention < ApplicationRecord
   belongs_to :mentioning_report, class_name: 'Report'
   belongs_to :mentioned_report, class_name: 'Report'
 
-  validates :mentioning_report, presence: true, uniqueness: { scope: :mentioned_report_id }
-  validates :mentioned_report, presence: true
+  validates :mentioning_report, uniqueness: { scope: :mentioned_report_id }
 end
